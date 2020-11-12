a180213c58d59195b123b5fefccfa47ed97bf297
Snowsune <kenwood364@gmail.com>
2020-11-12 02:10:20 -0500

    Fix all background tasks!

--- config.py
+++ config.py
@@ -19,13 +19,14 @@ def read_config(filename='config.ini', section='postgresql'):
 
     return db
 
-def write_config(filename='temp-config.ini,'):
+def default_config(filename='temp-config.ini,'):
     # create a parser
     parser = ConfigParser()
 
     # Setup Defaults
     postgresql_defaults = {"host":"localhost", "database":"bacula", "user":"bacula", "password":"password"}
     discord_defaults = {"token":"TOKEN", "bot":"True", "reconnect":"True"}
+    bacula_defaults = {"job_map":"/etc/bacula/job-map.ini"}
 
     # Add sections and keys
     parser.add_section('postgresql')
@@ -35,6 +36,10 @@ def write_config(filename='temp-config.ini,'):
     parser.add_section('discord')
     for key in discord_defaults.keys():
         parser.set('discord', key, discord_defaults[key])
+
+    parser.add_section('bacula')
+    for key in discord_defaults.keys():
+        parser.set('bacula', key, bacula_defaults[key])
 
     # Write files
     with open(filename, 'w') as f:
--- main.py
+++ main.py
@@ -1,43 +1,54 @@
 #!/usr/bin/python
-import discord, bacula
-from discord.ext import commands
-from config import read_config, write_config
+import discord, bacula, asyncio
+from discord.ext import commands, tasks
+from config import read_config, default_config
 
 
-# Prefixes the bot will respond to
-def get_prefix(bot, message):
-    prefixes = ['>', '!']
+class BatHook(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-# Commands the bot can run-once
-initial_extensions = []
+        self.bacula_connection = None
 
-# Create bot object
-bot = commands.Bot(command_prefix=get_prefix, description='A Robot made of tape.')
+        # create the background task and run it in the background
+        #self.monitor_db = self.loop.create_task(self.background_db_mon())
+        self.background_db_mon.start()
 
-# Create bacula object
-bacula_connection = None
+    async def on_ready(self):
+        # Runs when ready
+        print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+
+        await bot.change_presence(activity=discord.Game(name="In Debug mode!"))
+        print(f'Successfully logged in and booted...!')
+
+        print(f'Preparing Bacula backend...')
+        self.bacula_connection = bacula.Bacula()
+
+    @tasks.loop(seconds=5.0)
+    async def background_db_mon(self):
+        # Wait till we're ready before speaking.
+        await self.wait_until_ready()
+        counter = 0
+        channel = self.get_channel(666872163030007808) # channel ID goes here
+        #while not self.is_closed():
+        counter += 1
+        print(counter)
+        await channel.send(counter)
+        await asyncio.sleep(5) # task runs every 60 seconds
+
 
 if __name__ == '__main__':
     try:
         discord_config = read_config(filename='config.ini',
                                      section='discord')
     except Exception:
+        # This exception is only handled at startup, other missing sections wont be handled.
         print("The config.ini was malformed or could not be found, would you like the software to regenerate it? [Y/n]:")
         if input().lower() == 'y':
             write_config(filename="config.ini")
             quit()
 
-@bot.event
-async def on_ready():
-    # Runs when ready
-    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-
-    await bot.change_presence(activity=discord.Game(name="In Debug mode!"))
-    print(f'Successfully logged in and booted...!')
-
-    print(f'Preparing Bacula backend...')
-    bacula_connection = bacula.Bacula()
-
 
 # We have to pop the token because discord py treats it as positional, the rest of the args can be passed normally
+bot = BatHook()
 bot.run(discord_config.pop('token'), **discord_config) 
